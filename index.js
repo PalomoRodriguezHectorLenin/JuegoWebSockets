6e30f0d7eb1d82f9a2c7d03154b36b8444e9e14d
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-11-08 12:07:56 -0600

    JSON-Chat

---
+++ index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+var server = require('http').createServer(app);
+
+app.use(cors());
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//static files
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Empezar el servidor
+server.listen(app.get('port'), ()=>{
+    console.log('server on port', app.get('port'));
+})
+
+//Se inicia la comunicación
+
+const ScoketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log('new connection', socket.id);
+    
+    socket.on('chat:message', (data)=>{
+        io.sockets.emit('chat:message', data);
+    });
+    socket.on('chat:typing', (data)=>{
+        socket.broadcast.emit('chat:typing', data);
+    });
+});
---
+++ public/cliente.js
@@ -0,0 +1,36 @@
+var socket = io('http://192.168.100.16:3000/');
+
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let acitons = document.getElementById('actions');
+
+
+btn.addEventListener('click', function(){
+	socket.emit('chat:message', {
+		message: message.value,
+		username: username.value
+	})
+})
+
+message.addEventListener('keypress', function(){
+	socket.emit('chat:typing', username.value);
+});
+
+socket.on('chat:message', function(data){
+	acitons.innerHTML = '';
+	output.innerHTML += `<p> 
+		<strong>${data.username}</strong>: ${data.message}
+	</p>`
+});
+
+socket.on('chat:score', function(score){
+	actions.innerHTML = '';
+	output.innerHTML += `<p>
+	<strong>${data.username}</strong>: ${score}`
+});
+
+socket.on('chat:typing', function (data){
+	actions.innerHTML = `<p><en>${data} está escribiendo...</en></p>`
+});
