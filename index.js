40a533a19971af17f8787010e6dd12d2bca1b442
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-12-05 13:04:52 -0600

    GruposChat

--- index.js
+++ index.js
@@ -22,9 +22,21 @@ const io = SocketIO(server);
 
 io.on('connection', (socket)=>{
     console.log('new connection', socket.id);
+
+    var channel = 'canalA';
+
+    socket.join(channel);
+
+    socket.on('change channel', function(newChannel){
+        socket.leave(channel);
+        socket.join(newChannel);
+        channel = newChannel;
+        socket.emit('change channel', newChannel)
+    })
     
     socket.on('chat:message', (data)=>{
-        io.sockets.emit('chat:message', data);
+        //io.sockets.emit('chat:message', data);
+        io.sockets.in(channel).emit('chat:message', data);
     });
     socket.on('chat:typing', (data)=>{
         socket.broadcast.emit('chat:typing', data);
--- public/cliente.js
+++ public/cliente.js
@@ -1,4 +1,4 @@
-var socket = io('https://juegowebsockets-production.up.railway.app/');//https://juegowebsockets-production.up.railway.app/
+var socket = io('localhost:3000');//https://juegowebsockets-production.up.railway.app/
 
 let message = document.getElementById('message');
 let username = document.getElementById('username');
@@ -27,4 +27,12 @@ socket.on('chat:message', function(data){
 
 socket.on('chat:typing', function (data){
 	actions.innerHTML = `<p><en>${data} está escribiendo...</en></p>`
+});
+
+socket.on('change channel', function(channel){
+	$("#chat:message").html('').append($('<li>').text('system: bienvenido al canal' + channel + '!'));
+});
+
+$("#channel").change(function(){
+	socket.emit('change channel', $("#channel").val());
 });
